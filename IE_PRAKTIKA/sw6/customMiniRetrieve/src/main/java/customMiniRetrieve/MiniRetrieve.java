a4e6b5d089efddf2d7c220a397955be1871ea9da
mannhkai <mannhkai@students.zhaw.ch>
2021-11-02 20:04:53 +0100

    replaced String in invIndex with File

--- IE_PRAKTIKA/sw6/customMiniRetrieve/src/main/java/customMiniRetrieve/MiniRetrieve.java
+++ IE_PRAKTIKA/sw6/customMiniRetrieve/src/main/java/customMiniRetrieve/MiniRetrieve.java
@@ -15,36 +15,36 @@ public class MiniRetrieve {
     private static final String DOCUMENT_PATH = "documents/";
     private static final String QUERRY_PATH = "queries/";
 
-    private static Map<Integer, String> querries;
-    private static Map<Integer, String> documents;
-
-    private static Map<String, Map<String, Integer>> invIndex;
+    private static Map<Integer, File> querries;
+    private static Map<Integer, File> documents;
+    //word (fname->freq)
+    private static Map<String, Map<File, Integer>> invIndex;
     private static Map<File, String> nonInvIndex;
 
 
     public static void main(String[] args) {
-        querries = readFiles(readDirectory(QUERRY_PATH));
-        documents = readFiles(readDirectory(DOCUMENT_PATH));
+        querries = readDirectory(QUERRY_PATH);
+        documents = readDirectory(DOCUMENT_PATH);
         invIndex = new HashMap<>();
-        System.out.println(tokenizeString(querries.get(1)));
+        //System.out.println(tokenizeString(querries.get(1)));
         indexing(querries,documents);
 
     }
     //todo dont save documents as a string in hashmap
-    private static void indexing(Map<Integer, String> querries, Map<Integer, String> documents) {
-        for (String doc : documents.values()) {
-            List<String> terms = tokenizeString(doc);
+    private static void indexing(Map<Integer, File> querries, Map<Integer, File> documents) {
+        for (File doc : documents.values()) {
+            List<String> terms = tokenizeString(readFile(doc));
             for (String term : terms) {
                 //if invIndex already contains word and a doc already containing that term once increment
                 if (invIndex.containsKey(term) && invIndex.get(term).containsKey(doc)) {
-                    Map<String, Integer> mapOfTerm = invIndex.get(term);
+                    Map<File, Integer> mapOfTerm = invIndex.get(term);
                     mapOfTerm.put(doc, (mapOfTerm.get(doc) + 1));
                 // if invIndex contains word already but not with this doc put doc with count 1
                 } else if (invIndex.containsKey(term)) {
                     invIndex.get(term).put(doc, 1);
                 // if term is new in invIndex put it there with the doc it was found
                 } else {
-                    Map<String, Integer> newMap = new HashMap<>();
+                    Map<File,Integer> newMap = new HashMap<>();
                     newMap.put(doc, 1);
                     invIndex.put(term, newMap);
                 }
@@ -61,23 +61,20 @@ public class MiniRetrieve {
         return lowercasedToken;
     }
 
-    private static Map<Integer, String> readFiles(Map<Integer, File> files) {
-        Map<Integer, String> output = new HashMap<>();
-        for (Map.Entry<Integer, File> integerFileEntry : files.entrySet()) {
-            try (BufferedReader bf = new BufferedReader(new FileReader(integerFileEntry.getValue()))) {
+    private static String readFile(File file) {
+            String output = "";
+            try (BufferedReader bf = new BufferedReader(new FileReader(file))) {
                 String line = null;
                 StringBuilder sb = new StringBuilder();
                 while ((line = bf.readLine()) != null) {
                     sb.append(line);
                 }
-                output.put(integerFileEntry.getKey(), sb.toString());
+                output = sb.toString();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-        }
         return output;
     }
 
